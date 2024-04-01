b4f9b0f16c21816d3fdd1934c43e6700529c9efc
Patryk Śliwa <134144410+Waimaru@users.noreply.github.com>
2024-04-01 23:59:53 +0200

    Update main.jsx

--- myApp/src/main.jsx
+++ myApp/src/main.jsx
@@ -1,8 +1,14 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@mui/material';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import {theme} from 'theme';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
+);
